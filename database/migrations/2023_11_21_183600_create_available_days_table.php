61af90e60f9052e799f182082d79ffb7123cec06
danigracia-ion <146746314+danigracia-itb@users.noreply.github.com>
2023-11-22 15:22:04 +0100

    profile pic + user data

---
+++ database/migrations/2023_11_21_183600_create_available_days_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('available_days', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('monday')->default(false);
+            $table->boolean('tuesday')->default(false);
+            $table->boolean('wednesday')->default(false);
+            $table->boolean('thursday')->default(false);
+            $table->boolean('friday')->default(false);
+            $table->boolean('saturday')->default(false);
+            $table->boolean('sunday')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('available_days');
+    }
+};
---
+++ database/migrations/2023_11_21_184911_create_user_data_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_data', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("users");
+            $table->foreign('users')->references('id')->on('users')->onDelete('set null');
+
+            //P1
+            $table->date("date_birth");
+            $table->integer("height"); //cm
+            $table->float("body_weight", 5, 2);
+
+            //P2
+            $table->enum('time_training', ['0 - 6 months', '6 months - 1 year', '1 - 2 years', '+2 years']);
+            $table->time("train_available_time");
+
+            //Days
+            $table->unsignedBigInteger('available_days')->nullable();
+            $table->foreign('available_days')->references('id')->on('available_days')->onDelete('set null');
+
+            //P3
+            $table->text("wishlist_exercises");
+            $table->text("banlist_exercises");
+
+            //P4
+            $table->text("short_term_goals");
+            $table->text("long_term_goals");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_data');
+    }
+};
