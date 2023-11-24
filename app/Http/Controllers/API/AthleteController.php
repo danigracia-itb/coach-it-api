225e6fa7c3bfe3f8b90b61c28dd9feb8aef5b4d0
danigracia-ion <146746314+danigracia-itb@users.noreply.github.com>
2023-11-24 15:58:44 +0100

    save athlete form

---
+++ app/Http/Controllers/API/AthleteController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\UserData;
+use Illuminate\Http\Request;
+use App\Models\AvailableDays;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class AthleteController extends Controller
+{
+    //Guardar la user data
+    public function storeUserData(Request $request, string $id)
+    {
+        //Validar datos
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'user_id' => "required",
+                'date_birth' => "required",
+                'height' => "required",
+                'body_weight' => "required",
+                'time_training' => "required",
+                'train_available_time' => "required",
+                'wishlist_exercises' => "required",
+                'banlist_exercises' => "required",
+                'short_term_goals' => "required",
+                'long_term_goals' => "required",
+                'available_days' => "required"
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 401);
+        }
+
+        //Save
+        $userData = UserData::create([
+            'user_id' => $id,
+            'date_birth' => $request->date_birth,
+            'height' => $request->height,
+            'body_weight' => $request->body_weight,
+            'time_training' => $request->time_training,
+            'train_available_time' => $request->train_available_time,
+            'wishlist_exercises' => $request->wishlist_exercises,
+            'banlist_exercises' => $request->banlist_exercises,
+            'short_term_goals' => $request->short_term_goals,
+            'long_term_goals' => $request->long_term_goals,
+        ]);
+
+        $available_days = AvailableDays::create([
+            'monday' => $request->available_days->monday,
+            'tuesday' => $request->available_days->tuesday,
+            'wednesday' => $request->available_days->wednesday,
+            'thursday' => $request->available_days->thursday,
+            'friday' => $request->available_days->friday,
+            'saturday' => $request->available_days->saturday,
+            'sunday' => $request->available_days->sunday,
+        ]);
+
+        //Asignar días disponible
+        $userData->availableDays()->associate($available_days);
+        $userData->save();
+
+
+        return response()->json([
+            'success' => true,
+        ]);
+    }
+}
--- app/Http/Controllers/API/ExerciseController.php
+++ app/Http/Controllers/API/ExerciseController.php
@@ -17,14 +17,6 @@ class ExerciseController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
@@ -36,14 +28,6 @@ class ExerciseController extends Controller
      * Display the specified resource.
      */
     public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
     {
         //
     }
